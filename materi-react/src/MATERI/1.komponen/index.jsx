b476f2dc764f877c5b137640fba1eba13a33eb71
Richard_cyber <128294864+rihard-cyber@users.noreply.github.com>
2023-05-20 12:51:50 +0700

    origin

---
+++ materi-react/src/MATERI/1.komponen/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ClassComponent from "./pembahasan/ClassComponent";
+
+export default class Komponen extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <ClassComponent />
+            </div>
+        )
+    }
+}
---
+++ materi-react/src/MATERI/1.komponen/pembahasan/ClassComponent.jsx
@@ -0,0 +1,15 @@
+imporrt React from "react";
+
+class ClassComponent extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Komponen ini dibuat dengan class componet</h1>
+                <h2>Hello Dunia</h2>
+            </div>
+        )
+    }
+}
+
+export default ClassComponent;
